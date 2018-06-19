d0e11f375a6e96e193365ba6c8a9643524bb0de6
Victor Munoz <Vmunoz94@hotmail.com>
2018-06-19 12:11:12 -0700

    update project section

--- resumeWebsite/js/site.js
+++ resumeWebsite/js/site.js
@@ -55,3 +55,89 @@ $(document).ready(function() {
     stickyToggle(sticky, stickyWrapper, $(window));
   });
 });
+
+
+//animate click on scroll
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+     }, 500, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+});
+
+// make button active and change src files of images
+$(".projectLinks").on("mouseover", function(){
+     $(".projectLinks").removeClass("active");
+     if($(this).text() === "Color Game"){
+          $(this).addClass("active");
+          $("img").fadeOut(function(){
+               $("img").attr("src", "resumeWebsite/images/colorGame.png");
+               $("img").fadeIn();
+          });
+     }
+     else if($(this).text() === "Todo List"){
+          $(this).addClass("active");
+          $("img").fadeOut(function(){
+               $("img").attr("src", "resumeWebsite/images/todoList.png");
+               $("img").fadeIn();
+          });
+     }
+     else if($(this).text() === "Patatap"){
+          $(this).addClass("active");
+          $("img").fadeOut(function(){
+               $("img").attr("src", "resumeWebsite/images/patatap.png");
+               $("img").fadeIn();
+          });
+     }
+     else if($(this).text() === "Tank Wars"){
+          $(this).addClass("active");
+          $("img").fadeOut(function(){
+               $("img").attr("src", "resumeWebsite/images/tankWar.png");
+               $("img").fadeIn();
+          });
+     }
+     else if($(this).text() === "Galactic Mail"){
+          $(this).addClass("active");
+          $("img").fadeOut(function(){
+               $("img").attr("src", "resumeWebsite/images/galacticMail.png");
+               $("img").fadeIn();
+          });
+     }
+     else if($(this).text() === "Calculator"){
+          $(this).addClass("active");
+          $("img").fadeOut(function(){
+               $("img").attr("src", "resumeWebsite/images/calculator.png");
+               $("img").fadeIn();
+          });
+     }
+})
